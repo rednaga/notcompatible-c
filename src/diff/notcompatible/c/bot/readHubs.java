d9fd345fcc223ae7fdf1f265f38776101954e960
Tim Strazzere <Tim.Strazzere@mylookout.com>
2014-05-02 13:30:56 -0700

    Load udp/hub files generically from command line arg

--- src/diff/notcompatible/c/bot/readHubs.java
+++ src/diff/notcompatible/c/bot/readHubs.java
@@ -10,22 +10,14 @@ import diff.notcompatible.c.bot.objects.HubList;
 public class readHubs {
 	
 	public static HubList hubList;
-	public static HubList udpList;
 	
 	public static void main(String[] args) {
 		hubList = new HubList();
-		udpList = new HubList();
 		
-		loadHubList();
-		loadUDPHubList();
+		loadList(new File(args[0]));
 		
 		System.out.println("Hub List: ");
 		printList(hubList);
-		
-		System.out.println("");
-		
-		System.out.println("UDP Hub List: ");
-		printList(udpList);
 	}
 	
 	public static void printList(HubList list) {
@@ -36,9 +28,9 @@ public class readHubs {
 		}
 	}
 	
-	private static void loadHubList() {
+	private static void loadList(File list) {
         try {
-        	File hubListFile = new File("hl.bin");
+        	File hubListFile = list;
         	if(hubListFile.exists()) {
 	            InputStream fi = new FileInputStream(hubListFile);
 	            byte[] buffer = new byte[fi.available()];
@@ -51,30 +43,7 @@ public class readHubs {
 	            file.close();
 	            fi.close();
         	} else {
-            	System.err.println(" [!] No Hub list found - likely never created!");
-        	}
-        } catch (Exception exception) {
-			exception.printStackTrace();
-        }
-	}
-	
-	// Gets local "uhl.bin"
-	private static void loadUDPHubList() {
-        try {
-        	File udpHubListFile = new File("uhl.bin");
-        	if(udpHubListFile.exists()) {
-	            InputStream fi = new FileInputStream(udpHubListFile);
-	            byte[] buffer = new byte[fi.available()];
-	            DataInputStream file = new DataInputStream(fi);
-	
-	            file.read(buffer);
-	            udpList.loadFromRaw(buffer);
-	            System.out.println(" [*] Loaded [ " + udpList.count() + " ] hubs");
-	            
-	            file.close();
-	            fi.close();
-        	} else {
-            	System.err.println(" [!] No UDP Hub list found - likely never created!");
+            	System.err.println(" [!] No Hub list found!");
         	}
         } catch (Exception exception) {
 			exception.printStackTrace();
